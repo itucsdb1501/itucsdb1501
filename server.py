15ec1d57603d2f3f46486123f4b28020cb92f80d
sametayalti <ayalti@itu.edu.tr>
2015-11-24 17:18:22 +0200

    conflictler duzeltildi.

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
+<<<<<<< HEAD
 import datetime
 import json
 import os
 import psycopg2 as dbapi2
 import re
+
+=======
+import os
+import json
+import re
+import datetime
+import psycopg2 as dbapi2
+
+from flask.helpers import url_for
 
 from flask import Flask
 from flask import redirect
@@ -19,6 +29,7 @@ from anthem import Anthem
 from init import INIT
 
 app = Flask(__name__)
+
 
 
 def get_elephantsql_dsn(vcap_services):
@@ -121,9 +132,7 @@ def fixture_page(key_fixture):
                            current_time=now.ctime())
 
 
-
-
-
+#alper
 @app.route('/anthems', methods=['GET', 'POST'])
 def anthems_page():
     if request.method == 'GET':
@@ -156,6 +165,55 @@ def anthem_page(key_anthem):
     return render_template('anthem.html', anthem=anthem,
                            current_time=now.ctime())
 
+
+
+
+
+
+@app.route('/initdb')
+def initialize_database():
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor=connection.cursor()
+
+        #query="""DROP TABLE IF EXIST ANTHEM"""
+        #cursor.execute(query)
+
+        #
+        query="""CREATE TABLE ANTHEM (id INTEGER,name VARCHAR(15))"""
+        cursor.execute(query)
+        #
+        query="""CREATE TABLE LANGUAGES(id INTEGER,name VARCHAR(15))"""
+        cursor.execute(query)
+        query="""INSERT INTO ANTHEM (id,name) VALUES (0,'Istiklal Marsi')"""
+        cursor.execute(query)
+        query="""INSERT INTO ANTHEM (id,name) VALUES (1,'Deneme')"""
+        cursor.execute(query)
+
+        query="""UPDATE ANTHEM SET id=2 WHERE id=0"""
+        cursor.execute(query)
+
+        query="""DELETE FROM ANTHEM WHERE id=1"""
+        cursor.execute(query)
+
+        connection.commit()
+    return redirect(url_for('home_page'))
+
+@app.route('/deneme')
+def counter_page():
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor=connection.cursor()
+
+        query="SELECT name FROM ANTHEM"
+        cursor.execute(query)
+
+        isim=cursor.fetchone()[0]
+        return "%s" %isim
+
+
+#alper
+
+
+
 @app.route('/initdb')
 def init_db():
     initialize = INIT(app.config['dsn'])
@@ -169,13 +227,16 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
-
+    #alper
     VCAP_SERVICES = os.getenv('VCAP_SERVICES')
-    if VCAP_SERVICES is None:
-        app.config['dsn'] = """user='vagrant' password='vagrant' host='localhost' port=54321 dbname='itucsdb'"""
-    else:
+    if VCAP_SERVICES is not None:
         app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=54321 dbname='itucsdb'"""
+    #alper
     app.run(host='0.0.0.0', port=port, debug=debug)
+
 
 
 
